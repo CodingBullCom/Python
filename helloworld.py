3a572a18a69e66efab0f9ab445f9d6f4194cf175
Pawan <pawan@codingbull.com>
2017-02-17 22:23:52 +0530

    Added helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+# File: helloworld.py
+# Created on: 17th Feb 2017
+# Updated on: 17th Feb 2017
+# Author: Pawan Kumar Singh [pawan@codingbull.com]
+# http://www.codingbull.com/python   
+#
+# This source code is the part of tutorial series provided on http://www.codingbull.com/python and 
+# it can be used/ distributed without any restriction.  
+
+#!/usr/bin/python3.6
+
+print("Hello, World!")
+print("Hello, Python world!")
